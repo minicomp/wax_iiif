11653790e0a7a81c93bf86d4940d12c2fb0779ba
mnyrop <mn2802@columbia.edu>
2019-05-31 15:38:08 -0400

    dir instead of tmpdir for permissions

--- spec/wax_iiif/utilities/pdf_splitter_spec.rb
+++ spec/wax_iiif/utilities/pdf_splitter_spec.rb
@@ -5,10 +5,10 @@ describe WaxIiif::Utilities::PdfSplitter do
   context 'comparing' do
     it 'generates the proper number of files' do
       skip('skipping expensive tests') if ENV['SKIP_EXPENSIVE_TESTS']
-      Dir.mktmpdir do |dir|
-        results = WaxIiif::Utilities::PdfSplitter.split('./spec/data/test.pdf', output_dir: dir)
-        expect(results.count).to eq(3)
-      end
+      dir = 'tmp'
+      Dir.mkdir dir
+      results = WaxIiif::Utilities::PdfSplitter.split('./spec/data/test.pdf', output_dir: dir)
+      expect(results.count).to eq(3)
     end
   end
 end
