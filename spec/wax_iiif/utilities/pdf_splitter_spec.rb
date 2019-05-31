a00f60f8a471663c20f1f6f2e0b2aca9dd2b5f4e
mnyrop <mn2802@columbia.edu>
2019-05-31 15:48:58 -0400

    try another test dir (travis)

--- spec/wax_iiif/utilities/pdf_splitter_spec.rb
+++ spec/wax_iiif/utilities/pdf_splitter_spec.rb
@@ -5,7 +5,7 @@ describe WaxIiif::Utilities::PdfSplitter do
   context 'comparing' do
     it 'generates the proper number of files' do
       skip('skipping expensive tests') if ENV['SKIP_EXPENSIVE_TESTS']
-      dir = '.tmp'
+      dir = './pdf_temp'
       Dir.mkdir dir
       results = WaxIiif::Utilities::PdfSplitter.split('./spec/data/test.pdf', output_dir: dir)
       expect(results.count).to eq(3)
